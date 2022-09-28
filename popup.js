28341b46da06d233dcf0154ec13cf3b15531a1e1
Enoch Xu <enoch.t.xu@gmail.com>
2022-09-28 15:30:54 -0700

    whoops

--- popup.js
+++ popup.js
@@ -76,34 +76,34 @@ async function loginHttp(sid, passcode) {
     }
   );
 
-  const statusData = await statusResponse.text();
-  alert(statusData);
-  const testRes = await fetch('https://api-d594f029.duosecurity.com/frame/v4/oidc/exit', {
-    method: 'POST',
-    headers: {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-User': '?1',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"'
-    },
-    body: new URLSearchParams({
-        'sid': sid,
-        'txid': txid,
-        'factor': 'Duo Mobile Passcode',
-        'device_key': '',
-        '_xsrf': 'f47b30aefe894db4839c576f705af5c6',
-        'dampen_choice': 'false'
-    }),
-  });
+  // const statusData = await statusResponse.text();
+  // alert(statusData);
+  // const testRes = await fetch('https://api-d594f029.duosecurity.com/frame/v4/oidc/exit', {
+  //   method: 'POST',
+  //   headers: {
+  //       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  //       'Accept-Language': 'en-US,en;q=0.9',
+  //       'Cache-Control': 'max-age=0',
+  //       'Connection': 'keep-alive',
+  //       'Sec-Fetch-Dest': 'document',
+  //       'Sec-Fetch-Mode': 'navigate',
+  //       'Sec-Fetch-Site': 'same-origin',
+  //       'Sec-Fetch-User': '?1',
+  //       'Upgrade-Insecure-Requests': '1',
+  //       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+  //       'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+  //       'sec-ch-ua-mobile': '?0',
+  //       'sec-ch-ua-platform': '"macOS"'
+  //   },
+  //   body: new URLSearchParams({
+  //       'sid': sid,
+  //       'txid': txid,
+  //       'factor': 'Duo Mobile Passcode',
+  //       'device_key': '',
+  //       '_xsrf': 'f47b30aefe894db4839c576f705af5c6',
+  //       'dampen_choice': 'false'
+  //   }),
+  // });
   // const testRes = await fetch(
   //   "https://shb.ais.ucla.edu/shibboleth-idp/profile/SAML2/Redirect/SSO?execution=e22s3&_eventId_proceed=1",
   //   {
